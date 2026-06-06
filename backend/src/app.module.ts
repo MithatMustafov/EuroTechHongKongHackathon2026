592c3364fbdc0bb33fd53776b726933edf0afa0b
Felix Jahr <153926505+felixjahr@users.noreply.github.com>
2026-06-06 20:01:05 +0200

    create invoices and extraction nest js module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InvoicesModule } from './invoices/invoices.module';
+import { ExtractionModule } from './extraction/extraction.module';
 
 @Module({
-  imports: [],
+  imports: [InvoicesModule, ExtractionModule],
   controllers: [AppController],
   providers: [AppService],
 })

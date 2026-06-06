c79ebd64662c4a67b52d1262ce81b0d723b144f2
Felix Jahr <153926505+felixjahr@users.noreply.github.com>
2026-06-06 23:12:42 +0200

    add empty rail decision method

--- backend/src/rail-decision/rail-decision.module.ts
+++ backend/src/rail-decision/rail-decision.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { RailDecisionService } from './rail-decision.service';
 
-@Module({})
+@Module({
+  providers: [RailDecisionService],
+  exports: [RailDecisionService],
+})
 export class RailDecisionModule {}
